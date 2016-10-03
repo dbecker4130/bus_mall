3f77368a085199857f2e0f56fe51e14151b1fdc7
Daniel Becker <dbecker4130@gmail.com>
2016-10-03 13:50:09 -0700

    setting up data

---
+++ app.js
@@ -0,0 +1,46 @@
+//set up data
+var imgArray [];
+imgArray[0].src = './img/bag.jpg';
+imgArray[1].src = './img/banana.jpg';
+imgArray[2].src = './img/bathroom.jpg';
+imgArray[3].src = './img/boots.jpg';
+imgArray[4].src = './img/breakfast.jpg';
+imgArray[5].src = './img/bubblegum.jpg';
+imgArray[6].src = './img/chair.jpg';
+imgArray[7].src = './img/cthulhu.jpg';
+imgArray[8].src = './img/dog-duck.jpg';
+imgArray[9].src = './img/dragon.jpg';
+imgArray[10].src = './img/pen.jpg';
+imgArray[11].src = './img/pet-sweep.jpg';
+imgArray[12].src = './img/scissors.jpg';
+imgArray[13].src = './img/shark.jpg';
+imgArray[14].src = './img/sweep.png';
+imgArray[15].src = './img/tauntaun.jpg';
+imgArray[16].src = './img/unicorn.jpg';
+imgArray[17].src = './img/usb.gif';
+imgArray[18].src = './img/water-can.jpg';
+imgArray[19].src = './img/wine-glass.jpg';
+
+var clearImgArray [];
+
+
+
+//Define Actions
+
+function test () {
+  var r = Math.floor(Math.random() * 19) + 1;
+  var g = Math.floor(Math.random() * 19) + 1;
+  var b = Math.floor(Math.random() * 19) + 1;
+  random = "rgb("+r+","+g+","+b+") ";
+  var final = document.body.style.backgroundColor = random
+
+
+
+
+
+
+
+
+
+
+//Execute Actions
