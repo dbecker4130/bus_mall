bbed77505e006acdd952745cb6feb17444c19b02
Daniel Becker <dbecker4130@gmail.com>
2016-10-03 14:19:09 -0700

    variables set up

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 //set up data
 var imgArray [];
-imgArray[0].src = './img/bag.jpg';
-imgArray[1].src = './img/banana.jpg';
-imgArray[2].src = './img/bathroom.jpg';
-imgArray[3].src = './img/boots.jpg';
-imgArray[4].src = './img/breakfast.jpg';
-imgArray[5].src = './img/bubblegum.jpg';
-imgArray[6].src = './img/chair.jpg';
-imgArray[7].src = './img/cthulhu.jpg';
-imgArray[8].src = './img/dog-duck.jpg';
-imgArray[9].src = './img/dragon.jpg';
-imgArray[10].src = './img/pen.jpg';
-imgArray[11].src = './img/pet-sweep.jpg';
-imgArray[12].src = './img/scissors.jpg';
-imgArray[13].src = './img/shark.jpg';
-imgArray[14].src = './img/sweep.png';
-imgArray[15].src = './img/tauntaun.jpg';
-imgArray[16].src = './img/unicorn.jpg';
-imgArray[17].src = './img/usb.gif';
-imgArray[18].src = './img/water-can.jpg';
-imgArray[19].src = './img/wine-glass.jpg';
+
+var picZero = './img/bag.jpg';
+var picOne = './img/banana.jpg';
+var picTwo = './img/bathroom.jpg';
+var picThree = './img/boots.jpg';
+var picFour = './img/breakfast.jpg';
+var picFive = './img/bubblegum.jpg';
+var picSix = './img/chair.jpg';
+var picSeven = './img/cthulhu.jpg';
+var picEight = './img/dog-duck.jpg';
+var picNine = './img/dragon.jpg';
+var picTen = './img/pen.jpg';
+var picEleven = './img/pet-sweep.jpg';
+var picTwelve = './img/scissors.jpg';
+var picThirteen = './img/shark.jpg';
+var picFourteen = './img/sweep.png';
+var picSixteen = './img/unicorn.jpg';
+var picSeventeen = './img/usb.gif';
+var picEighteen = './img/water-can.jpg';
+var picNinteen = './img/wine-glass.jpg';
 
 var clearImgArray [];
 
@@ -27,12 +27,18 @@ var clearImgArray [];
 
 //Define Actions
 
-function test () {
-  var r = Math.floor(Math.random() * 19) + 1;
-  var g = Math.floor(Math.random() * 19) + 1;
-  var b = Math.floor(Math.random() * 19) + 1;
-  random = "rgb("+r+","+g+","+b+") ";
-  var final = document.body.style.backgroundColor = random
+function randomNumberGenerator () {
+  var choiceOne = Math.floor(Math.random() * 19) + 1;
+  var choiceTwo = Math.floor(Math.random() * 19) + 1;
+  var choiceThree = Math.floor(Math.random() * 19) + 1;
+
+  console.log(choiceOne);
+  console.log(choiceTwo);
+    console.log(choiceThree);
+
+  }
+
+  randomNumberGenerator();
 
 
 
